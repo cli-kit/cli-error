87f3e24e208ddb117bef6a3cf3c2661b7e2e599d
cyberfunk <hello@cyberfunk.org>
2014-01-31 12:59:45 +0800

    Assert on configure() - module method.

---
+++ test/unit/configure.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var clierr = require('../..'),
+  load = clierr.load,
+  definition = clierr.definition,
+  errors = clierr.errors;
+
+describe('cli-error:', function() {
+  beforeEach(function(done) {
+    clierr.clear();
+    done();
+  });
+  afterEach(function(done) {
+    clierr.clear();
+    done();
+  });
+  it('should load errors', function(done) {
+    var config = {
+      start: 256,
+      lang: 'en_us'
+    }
+    clierr(config);
+    expect(clierr.config.start).to.eql(config.start);
+    expect(clierr.config.lang).to.eql(config.lang);
+    expect(require('cli-locale').language).to.eql(config.lang);
+    done();
+  });
+})
